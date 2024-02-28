6c05b24b3310895f22ea7b66a3f8ec426dc31c01
Ares Mutemet <aresmute@gmail.com>
2024-02-28 13:20:58 +0300

    Initial Commit

---
+++ Sine Oscillator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Sine Oscillator
+//
+//  Created by Areş Mutemet on 28.02.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
